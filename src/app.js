572446ed8abbdebe1f60dd1726832e100839156c
Anthony Elison <tonyelison37@gmail.com>
2023-11-25 16:12:44 -0700

    Add POST /login endpoint with mock user that returns jwt token

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
-app.get('/api', (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.json({
     message: 'Hello, World!',
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  // mock user data
+  const user = {
+    id: 1,
+    username: 'tony',
+    email: 'test@email.com',
+  };
+
+  jwt.sign({ user }, 'secretkey', (err, token) => {
+    res.json({ token });
   });
 });
 
