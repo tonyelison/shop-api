be639c81d0a628246de0d3d8bd0305b2ff50825d
Anthony Elison <tonyelison37@gmail.com>
2023-12-09 17:43:34 -0700

    Move users endpoint callbacks to controller module

---
+++ src/controllers/users.js
@@ -0,0 +1,23 @@
+import asyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
+import User from '../models/user.js';
+
+const getById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).exec();
+  return res.json({ 'user-id': user._id });
+});
+
+const register = asyncHandler(async (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, async (err, passwordHash) => {
+    if (err) return next(err);
+
+    const user = new User({
+      username: req.body.username,
+      password: passwordHash,
+    });
+    await user.save();
+    return res.sendStatus(201);
+  });
+});
+
+export default { getById, register };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,36 +1,13 @@
-import asyncHandler from 'express-async-handler';
-import bcrypt from 'bcryptjs';
 import Router, { Method } from '../util/router.js';
-import User from '../models/user.js';
+import usersController from '../controllers/users.js';
 
 const router = new Router();
 
-router.add(
-  Method.GET,
-  asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id).exec();
-    return res.json({ 'user-id': user._id });
-  }),
-  {
-    path: '/:id',
-    isProtected: true,
-  },
-);
+router.add(Method.GET, usersController.getById, {
+  path: '/:id',
+  isProtected: true,
+});
 
-router.add(
-  Method.POST,
-  asyncHandler(async (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, async (err, passwordHash) => {
-      if (err) return next(err);
-
-      const user = new User({
-        username: req.body.username,
-        password: passwordHash,
-      });
-      await user.save();
-      return res.sendStatus(201);
-    });
-  }),
-);
+router.add(Method.POST, usersController.register);
 
 export default router;
